6c5d9855699ea86f80bb0cb6fcb686bdab2b4be3
Serroria <139000554+Serroria@users.noreply.github.com>
2024-09-01 19:02:37 +0700

    Add files via upload

---
+++ Mahasiswa/src/main/java/test/mahasiswa/Mahasiswa.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test.mahasiswa;
+
+/**
+ *
+ * @author meryl
+ */
+public class Mahasiswa {
+    String tes;
+    
+void membaca(){
+    System.out.println("Membaca");
+}
+ void nyontek(){
+    System.out.println("Nyontek");
+}
+
+void modifikasi(){
+    System.out.println("modif");
+}
+
+}
---
+++ Mahasiswa/src/main/java/test/mahasiswa/MahasiswaBeraksi.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test.mahasiswa;
+/**
+ *
+ * @author meryl
+ */
+public class MahasiswaBeraksi {
+   public static void main(String[] args){
+       Mahasiswa mhs1 = new Mahasiswa();
+       
+       mhs1.tes="yahaha";
+       mhs1.membaca();
+       mhs1.nyontek();
+       mhs1.modifikasi();
+       
+       
+   }
+}
